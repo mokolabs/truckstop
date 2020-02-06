c434746efc97201befea0eca61c0bb2320dfcc38
Patrick Crowley <patrick@mokolabs.com>
2020-02-06 12:01:36 -0800

    And again

--- lib/trucker.rb
+++ lib/trucker.rb
@@ -44,7 +44,7 @@ module Trucker
 
         errors.each do |error|
           puts "...................."
-          puts error.full_messages
+          puts error.inspect
           puts "...................."
         end
       end
