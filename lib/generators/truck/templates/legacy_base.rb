922c02acd96fcabe841c9bea12192639f3633201
Patrick Crowley <patrick@mokolabs.com>
2020-02-14 09:17:59 -0800

    Upgrade migrate method - Add support for returning errors - Improve code comments

--- lib/generators/truck/templates/legacy_base.rb
+++ lib/generators/truck/templates/legacy_base.rb
@@ -3,9 +3,20 @@ class LegacyBase < ActiveRecord::Base
   establish_connection :legacy
 
   def migrate
-    new_record = self.class.to_s.gsub(/Legacy/,'::').constantize.new(map)
-    new_record[:id] = self.id
+    # Grab new model class name
+    model = self.class.to_s.gsub(/Legacy/,'').constantize
+
+    # Build new model record
+    new_record = model.new(map)
+
+    # Set new model primary key value to LegacyModel primary key value
+    new_record[:id] = self[self.class.primary_key.to_sym]
+
+    # Migrate new model record
     new_record.save
+
+    # Return any errors
+    return record.errors if new_record.errors.count > 0
   end
 
 end
